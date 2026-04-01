810a54653cce213c7575847fd35f3d8c3f05fd58
bdtmhmdahmdghlab-max <bdtmhmdahmdghlab@gmail.com>
2026-04-01 20:34:44 +0300

    Enhance comments in config.py for clarity
    Added comments to clarify the purpose of environment variables.

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "2040")) # الـ ID الافتراضي الرسمي
+    # الكود سيبحث عن القيمة في Railway، وإذا لم يجدها سيستخدم القيمة الافتراضية
+    API_ID = int(os.environ.get("API_ID", "2040")) 
     API_HASH = os.environ.get("API_HASH", "b18441a1ed629410971e434e0639e79d")
-    # الـ Session الذي استخرجته من ترمكس
-    SESSION = os.environ.get("SESSION_STRING") 
-    # الـ ID الخاص بالمجموعة التي ستستقبل "الصيد" (يجب أن يبدأ بـ -100)
+    
+    # هذه القيم "حساسة" لذا نتركها بدون قيمة افتراضية ليتم جلبها من Railway فقط
+    SESSION_STRING = os.environ.get("SESSION_STRING") 
     TARGET_ID = int(os.environ.get("TARGET_ID", "-100123456789"))
-  
+
